6fc9767fadd53c4549dbd39ec615733028a42929
olga6680 <59262555+olga6680@users.noreply.github.com>
2020-07-17 21:39:40 +0300

    без коментария

---
+++ catalog/language/ua-uk/extension/module/pm_review.php
@@ -0,0 +1,16 @@
+<?php
+// Heading 
+$_['heading_title'] 	= 'Відгуки про товари';
+
+// Button
+$_['button_readmore']	= 'Докладніше →';
+$_['button_show_all']	= 'Показати все →';
+
+// Entry
+$_['entry_good']		= 'Переваги';
+$_['entry_bad']			= 'Недоліки';
+$_['entry_text']   		= 'Відгук';
+$_['entry_image']   	= 'Фотографії';
+
+$_['text_review_main_h4']			= 'Ось</br>що говорять';
+$_['text_review_main_p']			= 'Наші улюблені ♥ покупці:';
