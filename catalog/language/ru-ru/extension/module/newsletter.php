cf99c69b77d6dccfd4cd39bac35bfe7b71346738
olga6680 <59262555+olga6680@users.noreply.github.com>
2020-07-27 15:43:49 +0300

    Сообщение при вводе email
    Добавила сообщение об успешной подписке без ввода в админке. Добавила input в header. Он в изменении меню на закрытие категорий и контактов

--- catalog/language/ru-ru/extension/module/newsletter.php
+++ catalog/language/ru-ru/extension/module/newsletter.php
@@ -7,4 +7,7 @@ $_['button_unsubscribe']     = 'Отписаться';
 $_['error_email']     = 'Неправильный Email';
 $_['message_not_found']     = 'Указанного email нет в списке подписчиков';
 $_['message_unsubscribed']     = 'Вы отписались от подписки.';
-$_['message_exist']     = 'Этот email уже подписан на наши новости :)';
+$_['message_exist']     = 'Этот email уже подписан :)';
+
+$_['success_message']     = 'Спасибо за подписку :)';
+
