9d31c71098cc9a8954fcffbeb9eeede609466852
olga6680 <59262555+olga6680@users.noreply.github.com>
2020-08-03 16:36:14 +0300

    Баннер Новинки/Скидки

---
+++ catalog/language/ru-ru/extension/module/bannerhom.php
@@ -0,0 +1,9 @@
+<?php
+
+$_['text_new_bannerhom'] = 'Новинки';
+$_['text_sale_bannerhom'] = 'Скидки';
+
+$_['button_new_bannerhom']      = 'Смотреть все новинки';
+$_['button_sale_bannerhom']      = 'Смотреть товары со скидками';
+
+
